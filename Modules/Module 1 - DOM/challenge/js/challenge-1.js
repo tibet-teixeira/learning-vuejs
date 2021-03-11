3850b16cd5d503eaff44eaba9152a23ff467e8af
Tibet Teixeira <tibetbrasil@gmail.com>
2021-03-11 12:31:12 -0300

    It updates challenge 1

--- Modules/Module 1 - DOM/challenge/js/challenge-1.js
+++ Modules/Module 1 - DOM/challenge/js/challenge-1.js
@@ -1,6 +1,7 @@
 new Vue({
-    el: '#challenge',
+    el: '#challenge-1',
     data: {
+        title: 'VueJS Challenge #1',
         name: 'Tibet Teixeira',
         age: 22,
         vuejs_img: 'https://miro.medium.com/max/900/1*OrjCKmou1jT4It5so5gvOA.jpeg',
