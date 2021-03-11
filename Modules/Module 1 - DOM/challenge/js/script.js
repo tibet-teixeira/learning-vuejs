45f96b83174bbad4688c3a05595319697c32aa46
Tibet Teixeira <tibetbrasil@gmail.com>
2021-03-11 00:38:12 -0300

    It adds challenge 1

---
+++ Modules/Module 1 - DOM/challenge/js/script.js
@@ -0,0 +1,17 @@
+new Vue({
+    el: '#challenge',
+    data: {
+        name: 'Tibet Teixeira',
+        age: 22,
+        vuejs_img: 'https://miro.medium.com/max/900/1*OrjCKmou1jT4It5so5gvOA.jpeg',
+        input_value: 'Tibet'
+    },
+    methods: {
+        rand_number() {
+            return Math.floor(Math.random() * 10);
+        },
+        age_mult_3() {
+            return this.age * 3;
+        }
+    },
+})
